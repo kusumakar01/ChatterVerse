cb7e7ad1f2d50b9cd673d723f5a1167085a1f85d
kusumakar01 <ktanwar.cse@gmail.com>
2023-08-14 23:53:22 +0530

    Added Vanta.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -13,7 +13,6 @@ import { useHistory } from "react-router";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
 
-
 function Homepage() {
   const history = useHistory();
 
